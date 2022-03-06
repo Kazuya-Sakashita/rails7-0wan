43737e66e885354f45b9a95d328286063e9f8cae
Kazuya-Sakashita <kz0508@gmail.com>
2022-03-06 17:24:20 +0900

    devise実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+
+  #アカウント登録後のリダイレクト先
+  def after_sign_up_path_for(resource)
+    root_path
+  end
+
+  #アカウント編集後のリダイレクト先
+  def after_update_path_for(resource)
+    root_path
+  end
+
+  #ログイン後のリダイレクト先
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
+  #ログアウト後のリダイレクト先
+  def after_sign_out_path_for(resources)
+    root_path
+  end
 end
